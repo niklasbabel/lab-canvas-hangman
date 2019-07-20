80b749f2689b600d3b6b64918b5b50dd3da5b8d5
Niklas Babel <niklas@wane.co>
2019-07-20 16:43:56 +0200

    jasmine: 30 specs, 12 failures

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,56 @@
-var hangman;
+let hangman;
 
-// function Hangman() {
+class Hangman {
+  constructor(words) {
+    this.words = ["test", "test2", "test3"];
+    this.secretWord = "";
+    this.letters = [];
+    this.guessedLetter = "";
+    this.errorsLeft = 0;
+  }
 
-// }
+  checkIfLetter(keyCode) {
+    if (keyCode >= 65 && keyCode <= 90) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-// Hangman.prototype.getWord = function () {
+  checkClickedLetters(string) {
+    console.log(this);
+    if (this.letters.includes(string)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-// };
+  getWord() {
+    return this.words[Math.floor(Math.random() * this.words.length)];
+  }
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+  addCorrectLetter(string) {
+    this.guessedLetter = this.guessedLetter + string;
+    return this.guessedLetter;
+  }
+}
 
-// };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
-
-document.getElementById('start-game-button').onclick = function () {
+document.getElementById("start-game-button").onclick = function() {
   hangman = new Hangman();
 };
 
+document.onkeydown = function(e) {};
 
-document.onkeydown = function (e) {
-
-};
+class HangmanCanvas {
+  constructor(secretWord) {
+    this.ctx = document.getElementById("hangman").getContext("2d");
+  }
+  createBoard() {}
+  drawLines() {}
+  writeCorrectLetter(index) {}
+  writeWrongLetter(letter, errorsLeft) {}
+  drawHangman(shape) {}
+  gameOver() {}
+  winner() {}
+}
